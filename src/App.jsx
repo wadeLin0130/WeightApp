4c4570c48bd0ce8b95b0876106bc1358c53f932d
WeiDiLin <wade0130x@gmail.com>
2026-05-05 01:17:39 +0800

    修正 後端

--- src/App.jsx
+++ src/App.jsx
@@ -19,12 +19,12 @@ import { getFirestore, collection, doc, setDoc, onSnapshot, getDoc, getDocs } fr
 const firebaseConfig = typeof __firebase_config !== 'undefined' 
   ? JSON.parse(__firebase_config) 
   : {
-      apiKey: "VITE_FIREBASE_API_KEY",
-      authDomain: "VITE_FIREBASE_AUTH_DOMAIN",
-      projectId: "VITE_FIREBASE_PROJECT_ID",
-      storageBucket: "VITE_FIREBASE_STORAGE_BUCKET",
-      messagingSenderId: "VITE_FIREBASE_MESSAGING_SENDER_ID",
-      appId: "VITE_FIREBASE_APP_ID"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
     };
 
 const app = initializeApp(firebaseConfig);
